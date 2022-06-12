471ecde552d62c49f380e4f31941255740b7812b
tingirifistik <51286195+tingirifistik@users.noreply.github.com>
2022-06-12 14:06:19 +0300

    Update sendRequest.py

--- sendRequest.py
+++ sendRequest.py
@@ -24,13 +24,15 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Twitter: {Style.RESET_ALL}"+ self.notFound)
         except KeyError:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Twitter: {Style.RESET_ALL}https://twitter.com/{username}")
-        
+            return ("Twitter: https://twitter.com/" + username)
+             
     def GitHub(self, username):
         github = requests.get(f"https://github.com/{username}")
         if github.status_code == 404:
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}GitHub: {Style.RESET_ALL}"+ self.notFound)
         elif github.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}GitHub: {Style.RESET_ALL}https://github.com/{username}")
+            return ("GitHub: https://github.com/" + username)
         else:
             pass
         
@@ -40,6 +42,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}YazBel Forumu: {Style.RESET_ALL}"+ self.notFound)
         elif yazbel.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}YazBel Forumu: {Style.RESET_ALL}https://forum.yazbel.com/u/{username}")
+            return ("YazBel: https://forum.yazbel.com/u/" + username)
         else:
             pass
 
@@ -49,6 +52,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Youtube: {Style.RESET_ALL}"+ self.notFound)
         elif youtube.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Youtube: {Style.RESET_ALL}https://www.youtube.com/c/{username}")
+            return ("Youtube: https://www.youtube.com/c/" + username)        
         else:
             pass
         
@@ -58,6 +62,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Repl.it: {Style.RESET_ALL}"+ self.notFound)
         elif replit.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Repl.it: {Style.RESET_ALL}https://repl.it/@{username}")
+            return ("Repl.it: https://repl.it/@" + username)
         else:
             pass
 
@@ -67,6 +72,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}TryHackMe: {Style.RESET_ALL}"+ self.notFound)
         elif tryhackme.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}TryHackMe: {Style.RESET_ALL}https://tryhackme.com/p/{username}")
+            return ("TryHackMe: https://tryhackme.com/p/" + username)
         else:
             pass
         
@@ -76,6 +82,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Instagram: {Style.RESET_ALL}"+ self.notFound)
         elif instagram.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Instagram: {Style.RESET_ALL}https://www.instagram.com/{username}")
+            return ("Instagram: https://www.instagram.com/" + username)
         else:
             pass
 
@@ -86,14 +93,16 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Reddit: {Style.RESET_ALL}"+ self.notFound)
         except IndexError:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Reddit: {Style.RESET_ALL}https://www.reddit.com/user/{username}")
-            
+            return ("Reddit: https://www.reddit.com/user/" + username)
+        
     def Duolingo(self, username):
         duolingo = requests.get(f"https://www.duolingo.com/2017-06-30/users?username={username}&_=1642946994401")
         if len(duolingo.json()["users"]) == 0:
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Duolingo: {Style.RESET_ALL}"+ self.notFound)
         else:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Duolingo: {Style.RESET_ALL}https://www.duolingo.com/profile/{username}")
-
+            return ("Duolingo: https://www.duolingo.com/profile/" + username)
+        
     def Steam(self, username):
         steam = requests.get(f"https://steamcommunity.com/search/SearchCommunityAjax?text={username}&filter=users&sessionid=77fea0b7b970929072567862&steamid_user=false&page=1",
                         headers={"Cookie": "sessionid=77fea0b7b970929072567862; steamCountry=TR%7Caf00fe0ae4fab79aa1cff82212541978; timezoneOffset=10800,0"})
@@ -102,9 +111,11 @@ class UserChecker():
         if steam.json()["search_result_count"] == 1:
             html = BeautifulSoup(steam.json()["html"], "html.parser")
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Steam: {Style.RESET_ALL}"+html.find("a", {"class":"searchPersonaName"})["href"])
+            return ("Steam: " + html.find("a", {"class":"searchPersonaName"})["href"])
         if steam.json()["search_result_count"] > 1:
             html = BeautifulSoup(steam.json()["html"], "html.parser")
             print(f"[+] {Fore.LIGHTGREEN_EX}Steam: {Style.RESET_ALL}"+html.find("a", {"class":"searchPersonaName"})["href"]+"  "+self.sentence)
+            return ("Steam: " + html.find("a", {"class":"searchPersonaName"})["href"]+"  "+self.sentence)
 
     def Lichess(self, username):
         lichess = requests.get(f"https://lichess.org/@/{username}")
@@ -112,6 +123,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Lichess: {Style.RESET_ALL}"+ self.notFound)
         elif lichess.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Lichess: {Style.RESET_ALL}https://lichess.org/@/{username}")
+            return ("Lichess: https://lichess.org/@/" + username)
         else:
             pass
 
@@ -121,6 +133,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}HackerOne: {Style.RESET_ALL}"+ self.notFound)
         elif hackerone.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}HackerOne: {Style.RESET_ALL}https://hackerone.com/{username}")
+            return ("HackerOne: https://hackerone.com/" + username)
         else:
             pass
         
@@ -131,15 +144,17 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}SnapChat: {Style.RESET_ALL}"+ self.notFound)
         if snap.json()["reference"]["status_code"] == "TAKEN":
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}SnapChat: {Style.RESET_ALL}https://www.snapchat.com/add/{username}")
+            return ("SnapChat: https://www.snapchat.com/add/" + username)
         else:
             pass
         
     def TikTok(self, username):
-        tiktok = requests.get(f"https://www.tiktok.com/@{username}")
-        if tiktok.status_code == 404:
+        tt = requests.head("https://www.tiktok.com/@"+username, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0", "accept-language": "en-US"})
+        if tt.status_code == 404:
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}TikTok: {Style.RESET_ALL}"+ self.notFound)
-        elif tiktok.status_code == 200:
+        elif tt.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}TikTok: {Style.RESET_ALL}https://www.tiktok.com/@{username}")
+            return ("TikTok: https://www.tiktok.com/@" + username)
         else:
             pass
 
@@ -151,8 +166,10 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}StackOverFlow: {Style.RESET_ALL}"+ self.notFound)
         elif len(profiles) == 1:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}StackOverFlow: {Style.RESET_ALL}https://stackoverflow.com"+profiles[0].find("a")["href"])
+            return ("StackOverFlow: https://stackoverflow.com"+profiles[0].find("a")["href"])
         elif len(profiles) > 1:
             print(f"[+] {Fore.LIGHTGREEN_EX}StackOverFlow: {Style.RESET_ALL}https://stackoverflow.com"+profiles[0].find("a")["href"]+"  "+self.sentence)
+            return ("StackOverFlow: https://stackoverflow.com"+profiles[0].find("a")["href"]+"  "+self.sentence)
         
     def Pinterest(self, username):
         pinterest = requests.get(f"https://pinterest.com/{username}")
@@ -160,6 +177,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Pinterest: {Style.RESET_ALL}"+ self.notFound)
         elif pinterest.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Pinterest: {Style.RESET_ALL}https://pinterest.com/{username}")
+            return ("Pinterest: https://pinterest.com/" + username)
         else:
             pass
         
@@ -169,6 +187,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}VK: {Style.RESET_ALL}"+ self.notFound)
         elif vk.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}VK: {Style.RESET_ALL}https://vk.com/{username}")
+            return ("VK: https://vk.com/" + username)
         else:
             pass
         
@@ -178,6 +197,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Medium: {Style.RESET_ALL}"+ self.notFound)
         elif medium.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Medium: {Style.RESET_ALL}https://medium.com/@{username}")
+            return ("Medium: https://medium.com/@" + username)
         else:
             pass
     
@@ -186,6 +206,7 @@ class UserChecker():
         twitch = requests.post("https://gql.twitch.tv/gql", data=dumps(data), headers={"Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"})
         if twitch.json()["data"]["streamPlaybackAccessToken"] != None:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}Twitch: {Style.RESET_ALL}https://twitch.tv/{username}")
+            return ("Twitch: https://twitch.tv/" + username)
         elif twitch.json()["data"]["streamPlaybackAccessToken"] == None:
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}Twitch: {Style.RESET_ALL}"+ self.notFound)
         else:
@@ -197,6 +218,7 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}BuyMeACoffee: {Style.RESET_ALL}"+ self.notFound)
         elif coffee.status_code == 200:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}BuyMeACoffee: {Style.RESET_ALL}https://www.buymeacoffee.com/{username}")
+            return ("BuyMeACoffee: https://www.buymeacoffee.com/" + username)
         else:
             pass
     
@@ -207,7 +229,8 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}EksiSozluk: {Style.RESET_ALL}"+ self.notFound)
         except AttributeError:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}EksiSozluk: {Style.RESET_ALL}https://eksisozluk.com/biri/{username}")
-            
+            return ("EksiSozluk: https://eksisozluk.com/biri/" + username)
+    
     def inciSozluk(self, username):
         inci = BeautifulSoup(requests.get("http://incisozluk.com.tr:80/u/"+username).content, "html.parser")
         try:
@@ -215,3 +238,4 @@ class UserChecker():
             print(f"[{Fore.LIGHTRED_EX}-{Style.RESET_ALL}] {Fore.LIGHTRED_EX}InciSozluk: {Style.RESET_ALL}"+ self.notFound)
         except AttributeError:
             print(f"[{Fore.LIGHTGREEN_EX}+{Style.RESET_ALL}] {Fore.LIGHTGREEN_EX}InciSozluk: {Style.RESET_ALL}http://incisozluk.com.tr/u/{username}")
+            return ("InciSozluk: http://incisozluk.com.tr/u/" + username)
